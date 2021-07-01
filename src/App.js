4370ca1eaa8f921189034e3e34654891d7c1720c
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-07-01 09:02:17 -0500

    Add basic search function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App"> Hello
+    <div className="App">
+      <div className="container">
+        <header className="App-header">WantsNewPaint WebDesign</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"><small>Coded by Bridget Young</small></footer>
+      </div>
     </div>
   );
 }
