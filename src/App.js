e0de3576a4962bb85658fbf2afd02759dfeb628a
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-07-02 08:36:25 -0500

    Added open source code link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="App-footer"><small>Coded by Bridget Young</small></footer>
+        <footer className="App-footer"><small><a href="https://github.com/Wantsnewpaint/dictionary-project" target="_blank" rel="noreferrer">Open-source</a> code by Bridget Young</small></footer>
       </div>
     </div>
   );
