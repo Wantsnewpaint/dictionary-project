1fae405e10da67cb936acbdc6125c18e7bf8b4c9
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-07-09 15:41:16 -0500

    Added photos and adjusted styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">WantsNewPaint WebDesign</header>
+        <header className="App-header">Dictionary Word Search</header>
         <main>
-          <Dictionary defaultKeyword="salutation" />
+          <Dictionary defaultKeyword="travel" />
         </main>
         <footer className="App-footer"><small><a href="https://github.com/Wantsnewpaint/dictionary-project" target="_blank" rel="noreferrer">Open-source</a> code by Bridget Young</small></footer>
       </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleWordResponse(response) {
         setResults(response.data[0]);
+    }
+
+    function handlePexelResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
                 //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleWordResponse);
+
+    //documentation for picture API: https://www.pexels.com/
+    let pexelsApiKey="563492ad6f9170000100000189be1363623543c68349587d7e7a288b";
+    let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelResponse);
     }
 
     function handleSubmit(event) {
@@ -46,6 +58,7 @@ return (
         </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
     </div>
     );
     } else {
