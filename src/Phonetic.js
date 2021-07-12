d9d828719a64b52cc815a34e783ae6be2d017d09
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-07-12 08:30:03 -0500

    Added in page audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,20 @@
 import React from "react";
-import "./Phonetic.css"
+import "./Phonetic.css";
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function Phonetic(props) {
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-                Listen
-            </a>
-            {props.phonetic.text}
+
+<ReactAudioPlayer
+  src={props.phonetic.audio}
+  autoPlay={false}
+  controls
+  className="audio"
+/>
+            
+<br />
+            <span className="text">{props.phonetic.text}</span>
         </div>
     )
 }
